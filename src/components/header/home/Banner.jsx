a08c1f460c57e23e5d2065dec10adc346baffbe2
SnehaSoni06 <snehasonivns@gmail.com>
2024-06-15 12:11:59 +0530

    banner complete

--- src/components/header/home/Banner.jsx
+++ src/components/header/home/Banner.jsx
@@ -1,8 +1,13 @@
 
 import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import {styled} from '@mui/material';
 import { bannerData } from '../../../constants/data';
 
-
+const Image=styled('img')({
+  width:'100%',
+  height:280
+})
 const responsive = {
     desktop: { 
       breakpoint: { max: 3000, min: 1024 },
@@ -21,10 +26,23 @@ const responsive = {
 const Banner=()=>{
     return (
         
-        <Carousel responsive={responsive}>
+        <Carousel 
+        responsive={responsive}
+        swipeable={false}
+        draggable={false}
+        infinite={true}
+        autoPlay={true}
+        autoPlaySpeed={4000}
+        keyBoardControl={true}
+        slidesToSlide={1}
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+        containerClass="carousel-container"
+        
+        >
         {
           bannerData.map((data, index) => (
-            <img 
+            <Image 
               key={index}
               src={data.url} 
               alt="banner" 
